1fd229a08225febf56747923d9ad542138145a4a
Shashank J Shetty <shettyshashank346@gmail.com>
2023-07-27 20:55:24 +0530

    Add new and create form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,18 @@
 class ArticlesController < ApplicationController
     def show
-        @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
+    end
+    def index
+      @articles = Article.all
+    end
+
+    def new
+        
+    end
+
+    def create
+        @new_article=Article.new(params.require(:article).permit(:title,:description))
+        @new_article.save
+        redirect_to @new_article
     end
 end
